1517209695080d8d05ce349d33b3d8814b80b708
KPswitch <itskenpride@gmail.com>
2023-02-06 18:28:08 -0800

    Add error-handling middleware for common errors and debugging

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
 
+router.use('/api', apiRouter);
 
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
 //   res.send('Hello World!');
 // });
+//
 
 router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
@@ -14,5 +17,7 @@ router.get("/api/csrf/restore", (req, res) => {
       'XSRF-Token': csrfToken
     });
   });
+
+
 
 module.exports = router;
