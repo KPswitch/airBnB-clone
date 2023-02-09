ff821b564f6b80893cce23dabb39fb091af4f2d1
KPswitch <itskenpride@gmail.com>
2023-02-08 19:51:43 -0800

    Add user login backend endpoint

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
 //const { setTokenCookie } = require('../../utils/auth.js');
 //const { User } = require('../../db/models');
-
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
 //const { requireAuth } = require('../../utils/auth.js');
 
 router.use(restoreUser);
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+  });
 
 // router.get(
 //   '/restore-user',
@@ -13,9 +20,6 @@ router.use(restoreUser);
 //     return res.json(req.user);
 //   });
 
-// router.post('/test', function(req, res) {
-//     res.json({ requestBody: req.body });
-//   });
 
 //   router.get('/set-token-cookie', async (_req, res) => {
 //     const user = await User.findOne({
