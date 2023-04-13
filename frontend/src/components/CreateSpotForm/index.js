9c7278f81db046ca8e8b5ef918ca0dfdf6166d8b
KPswitch <itskenpride@gmail.com>
2023-04-12 23:12:01 -0700

    build creaet spot form

--- frontend/src/components/CreateSpotForm/index.js
+++ frontend/src/components/CreateSpotForm/index.js
@@ -14,7 +14,7 @@ const CreateSpotComponent = () => {
         lat:'',
         lng:'',
         description:'',
-        title:'',
+        name:'',
         price:'',
         previewImage:'',
         imageurl1:'',
@@ -55,7 +55,7 @@ const CreateSpotComponent = () => {
     return (
         <div>
           <h1>Create New Spot </h1>
-          <h3>Where's your place located?</h3>
+          <h2>Where's your place located?</h2>
           <p>Guest will only get your exact address once they booked a reservation</p>
           <form onSubmit={handleSubmit}>
             <label>
@@ -123,7 +123,99 @@ const CreateSpotComponent = () => {
                 />
             </label>
             <div style={{ borderBottom: '1px solid black', paddingBottom: '10px'}}></div>
+            <h2>Describe your place to guests</h2>
+            <p>Mention the best features of your space, any special amentites like fast wifi or parking, and what you love about the neighborhood.</p>
+            <label>
+
+                <textarea
+
+                name="description"
+                placeholder="Please write at least 30 characters"
+                value={spotData.description}
+                onChange={handleChange}
+                />
+            </label>
+            <div style={{ borderBottom: '1px solid black', paddingBottom: '10px'}}></div>
+            <h2>Create a title for your spot</h2>
+            <p>Catch guest' attention with a spot title that highlights what makes your place special.</p>
+            <label>
+                name:
+                <input
+                type="text"
+                name="name"
+                placeholder="name of your spot"
+                value={spotData.name}
+                onChange={handleChange}
+                />
+            </label>
+            <div style={{ borderBottom: '1px solid black', paddingBottom: '10px'}}></div>
+        ,   <h2>Set a abse price for your spot</h2>
+            <p>Competive pricing can help your listing stand out and rank higher in search results</p>
+            <label>
+                $
+                <input
+                type="text"
+                name="price"
+                placeholder="Price per night (USD)"
+                value={spotData.price}
+                onChange={handleChange}
+                />
+            </label>
+            <div style={{ borderBottom: '1px solid black', paddingBottom: '10px'}}></div>
+            <h2>Liven up your spot with photos</h2>
+            <p>Submit a link to at least one photo to publish your spot</p>
+            <label>
+                <input
+                type="text"
+                name="previewImage"
+                placeholder="Preview image URL"
+                value={spotData.previewImage}
+                onChange={handleChange}
+                />
+            </label>
+            <br />
+            <label>
+                <input
+                type="text"
+                name="imageurl1"
+                placeholder="Image URL"
+                value={spotData.imageurl1}
+                onChange={handleChange}
+                />
+            </label>
+            <br />
+            <label>
+                <input
+                type="text"
+                name="imageurl2"
+                placeholder="Image URL"
+                value={spotData.imageurl2}
+                onChange={handleChange}
+                />
+            </label>
+            <br />
+            <label>
+                <input
+                type="text"
+                name="imageurl3"
+                placeholder="Image URL"
+                value={spotData.imageurl3}
+                onChange={handleChange}
+                />
+            </label>
+            <br />
+            <label>
+                <input
+                type="text"
+                name="imageurl4"
+                placeholder="Image URL"
+                value={spotData.imageurl4}
+                onChange={handleChange}
+                />
+            </label>
             </form>
+            <div style={{ borderBottom: '1px solid black', paddingBottom: '10px'}}></div>
+
           <button type="submit">Create Spot</button>
 
 
